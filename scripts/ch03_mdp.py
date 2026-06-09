c9803bd500098a3ea8416c80b40d554778c71972
YiKejun <ykj42960@163.com>
2026-06-09 19:58:31 +0800

    add occupancy measure

--- scripts/ch03_mdp.py
+++ scripts/ch03_mdp.py
@@ -213,3 +213,30 @@ print("使用蒙特卡洛方法计算MDP的状态价值为\n", V)
 # 使用蒙特卡洛方法计算MDP的状态价值为
 #  {'s1': -1.228923788722258, 's2': -1.6955696284402704, 's3': 0.4823809701532294,
 # 's4': 5.967514743019431, 's5': 0}
+
+"""
+occupancy measure
+"""
+def occupancy(episodes, s, a, timestep_max, gamma):
+    ''' 计算状态动作对（s,a）出现的频率,以此来估算策略的占用度量 '''
+    rho = 0
+    total_times = np.zeros(timestep_max)  # 记录每个时间步t各被经历过几次
+    occur_times = np.zeros(timestep_max)  # 记录(s_t,a_t)=(s,a)的次数
+    for episode in episodes:
+        for i in range(len(episode)):
+            (s_opt, a_opt, r, s_next) = episode[i]
+            total_times[i] += 1
+            if s == s_opt and a == a_opt:
+                occur_times[i] += 1
+    for i in reversed(range(timestep_max)):
+        if total_times[i]:
+            rho += gamma**i * occur_times[i] / total_times[i]
+    return (1 - gamma) * rho
+
+timestep_max = 1000
+
+episodes_1 = sample(MDP, Pi_1, timestep_max, 1000)
+episodes_2 = sample(MDP, Pi_2, timestep_max, 1000)
+rho_1 = occupancy(episodes_1, "s4", "概率前往", timestep_max, gamma)
+rho_2 = occupancy(episodes_2, "s4", "概率前往", timestep_max, gamma)
+print(rho_1, rho_2)
