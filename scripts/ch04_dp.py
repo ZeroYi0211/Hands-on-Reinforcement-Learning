bb8c4b8959331dc4d69a07beb1fdc623059f0043
YiKejun <ykj42960@163.com>
2026-07-01 23:48:54 +0800

    complete ch04 policy_iteration

--- scripts/ch04_dp.py
+++ scripts/ch04_dp.py
@@ -27,15 +27,15 @@ class CliffWalkingEnv:
                         continue
                     # 其他位置
                     next_x = min(self.ncol - 1, max(0, j + change[a][0]))
-                    next_y = min(self.nrow - 1, max(0, j + change[a][1]))
+                    next_y = min(self.nrow - 1, max(0, i + change[a][1]))
                     next_state = next_y * self.ncol + next_x # 状态索引 = 行 * 12 + 列
                     reward = -1
                     done = False
                     # 下一个位置在悬崖或者终点
                     if next_y == self.nrow - 1 and next_x > 0:
                         done = True
-                        if next_x != self.ncol - 1 # 下一个位置在悬崖
-                        reward = -100
+                        if next_x != self.ncol - 1: # 下一个位置在悬崖
+                            reward = -100
                     P[i * self.ncol + j][a] = [(1, next_state, reward, done)]
         return P
     
@@ -47,5 +47,87 @@ class PolicyIteration:
         self.v = [0] * self.env.nrow * self.env.ncol # 初始化所有状态的价值为0
         self.pi = [[0.25, 0.25, 0.25,0.25]
                    for i in range(self.env.ncol * self.env.nrow)] # 初始化为均匀随即策略
-        self.theta = theta
-        self.gamma = gamma
+        self.theta = theta # 策略评估收敛阈值
+        self.gamma = gamma # 折扣因子
+
+    def policy_evaluation(self): # 策略评估
+        cnt = 1
+        while 1:
+            max_diff = 0
+            new_v = [0] * self.env.ncol * self.env.nrow
+            for s in range(self.env.ncol * self.env.nrow):
+                qsa_list = []
+                for a in range(4):
+                    qsa = 0
+                    for res in self.env.P[s][a]:
+                        p, next_state, r, done = res
+                        qsa += p * (r + self.gamma * self.v[next_state] * 
+                                    (1 - done))
+                    qsa_list.append(self.pi[s][a] * qsa)
+                new_v[s] = sum(qsa_list)
+                max_diff = max(max_diff, abs(new_v[s] - self.v[s]))
+            self.v = new_v
+            if max_diff < self.theta: break
+            cnt += 1
+        print("策略评估进行%d轮后完成" % cnt)
+
+    def policy_improvement(self): # 策略提升
+        for s in range(self.env.nrow * self.env.ncol):
+            qsa_list = []
+            for a in range(4):
+                qsa = 0
+                for res in self.env.P[s][a]:
+                    p, next_state, r, done = res
+                    qsa += p * (r + self.gamma * self.v[next_state] * 
+                                (1 - done))
+                qsa_list.append(qsa)
+            maxq = max(qsa_list)
+            cntq = qsa_list.count(maxq)
+            self.pi[s] = [1 / cntq if q == maxq else 0 for q in qsa_list]
+        print("策略提升完成")
+        return self.pi
+    
+    def policy_iteration(self): # 策略迭代
+        while 1:
+            self.policy_evaluation()
+            old_pi = copy.deepcopy(self.pi)
+            '''
+            这里深拷贝的作用是递归地创建一个对象的完全独立副本
+            python中的 b = a 是引用赋值 直接赋值指向的还是同一个对象
+            '''
+            new_pi = self.policy_improvement()
+            if old_pi == new_pi: break
+
+def print_agent(agent, action_meaning, disaster=[], end=[]):
+    print("状态价值：")
+    for i in range(agent.env.nrow):
+        for j in range(agent.env.ncol):
+            # 为了输出美观,保持输出6个字符
+            print('%6.6s' % ('%.3f' % agent.v[i * agent.env.ncol + j]),
+                  end=' ')
+        print()
+
+    print("策略：")
+    for i in range(agent.env.nrow):
+        for j in range(agent.env.ncol):
+            # 一些特殊的状态,例如悬崖漫步中的悬崖
+            if (i * agent.env.ncol + j) in disaster:
+                print('****', end=' ')
+            elif (i * agent.env.ncol + j) in end:  # 目标状态
+                print('EEEE', end=' ')
+            else:
+                a = agent.pi[i * agent.env.ncol + j]
+                pi_str = ''
+                for k in range(len(action_meaning)):
+                    pi_str += action_meaning[k] if a[k] > 0 else 'o'
+                print(pi_str, end=' ')
+        print()
+
+
+env = CliffWalkingEnv()
+action_meaning = ['^', 'v', '<', '>']
+theta = 0.001
+gamma = 0.9
+agent = PolicyIteration(env, theta, gamma)
+agent.policy_iteration()
+print_agent(agent, action_meaning, list(range(37, 47)), [47])
